08296d583c936edf44ad18adf2a4119220d4a6ff
DuilioMF <57972694+DuilioMF@users.noreply.github.com>
2026-05-07 10:12:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
   btn.innerHTML = '<span>Enviando...</span> <i class="fas fa-spinner fa-spin"></i>';
   btn.disabled = true;
 
-  fetch("https://formspree.io/dmfpersonal@gmail.com", { 
+  fetch("https://formspree.io/f/mwvyqgoy", { 
     method: "POST",
     body: data,
     headers: {
